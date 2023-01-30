cc34f766a985f17d8f27bfae623a2445461e2147
michaeldeven <michael.deven1999@gmail.com>
2023-01-30 15:17:05 +0700

    Test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/app/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
